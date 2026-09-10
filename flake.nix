4ff2af680d4008b4f2a0df5eedec158a9e6bc34f
syntacticallyazure <95119739+syntacticallyazure@users.noreply.github.com>
2026-09-10 16:13:31 +1000

    added scoop alias

--- flake.nix
+++ flake.nix
@@ -68,6 +68,7 @@
               alias neofetch='fastfetch'
               alias aria2c='aria2c --seed-time=0'
               alias where='which'
+              alias scoop='echo "Hey! You are on Unix, not Windows!"'
             '';
           };
         }
