1e1463a7fea64ecb5d037159304d1fc34e2594a8
syntacticallyazure <95119739+syntacticallyazure@users.noreply.github.com>
2026-09-23 17:14:05 +1000

    useful aliases

--- flake.nix
+++ flake.nix
@@ -105,6 +105,10 @@
               alias aria2c='aria2c --seed-time=0'
               alias where='which'
               alias tor='tor --HTTPTunnelPort 8118'
+
+              alias mv='mv -v';
+              alias cp='cp -v';
+              alias rm='rm -v';
             '';
           };
         }
